4a04defb407f536a6e7eee5f8fa6aae33f6d4f85
kareza <kareza@qq.com>
2023-04-23 00:35:22 +0800

    更新command文章

--- build_html.sh
+++ build_html.sh
@@ -1,1 +1,2 @@
 pandoc --embed-resources --standalone --metadata title="kareza 的技术博客" --css pandoc.css README.md --output docs/index.html
+pandoc --embed-resources --standalone --css pandoc.css posts/command.md --output docs/posts/command.html
