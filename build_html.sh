d64fabcef83e4bc301db650721c6985ea9351ef5
kareza <kareza@qq.com>
2023-04-23 00:17:19 +0800

    更新title，并修改pandoc参数

--- build_html.sh
+++ build_html.sh
@@ -1,1 +1,1 @@
-pandoc --standalone --self-contained --metadata title="挖个坑，目标：超过10w阅读" --css pandoc.css README.md --output html/index.html
+pandoc --embed-resources --standalone --metadata title="kareza 的技术博客" --css pandoc.css README.md --output docs/index.html
